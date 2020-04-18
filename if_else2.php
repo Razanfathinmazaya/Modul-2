4f194ffe41215bba8a0e3452e11840b432de51f7
Razanfathinmazaya <63759967+Razanfathinmazaya@users.noreply.github.com>
2020-04-18 09:16:37 +0700

    Update if_else2.php

--- if_else2.php
+++ if_else2.php
@@ -1,7 +1,7 @@
 <?php
 $user = "razan";
 $pass = "12345"
-if ($user == "alfandy" && $pass == "12345") {
+if ($user == "razan" && $pass == "12345") {
 	echo "Login Berhasil";
 } else {
 	echo "Login Gagal";
